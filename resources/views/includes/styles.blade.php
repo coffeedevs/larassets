1771ed33e80be7a5d5b2b33ae7be416d800a7fe8
Hijos <Hijos>
2016-02-07 19:20:35 -0300

    Fix animations, fixed buttons, fixed mobile layout!

--- resources/views/includes/styles.blade.php
+++ resources/views/includes/styles.blade.php
@@ -2,5 +2,8 @@
       integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('css/coffeedevs.css') }}">
 <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+<link rel="stylesheet" href="{{ asset('css/tether.min.css') }}">
 <link href='https://fonts.googleapis.com/css?family=Source+Code+Pro' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+<script src="//fast.eager.io/TKH5lNTYqp.js"></script>
